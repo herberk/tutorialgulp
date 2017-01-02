3b01a05ef39d2e7695a5981045fbca80f6684e95
Hermann Berkhoff <Hermann Berkhoff>
2017-01-02 10:41:50 -0300

    commit despues de retomr el capitulo 4

---
+++ Gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssnano = require('gulp-cssnano'),
+    argv =  require('yargs').argv,
+    gulpif = reguire('gulp-if');
+
+
+var isProduction;
+if(argv.prod) {
+    isProduction = true;
+} else {
+    isProduction = false;
+}
+var config = {
+    scssDir: './assets/scss',
+    cssDir: './assets/css'
+};
+
+gulp.task('style', function(){
+    gulp.src(config.scssDir + '/*.scss')
+   .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulpif(isProduction, cssnano(), sourcemaps.write('maps')))
+    .pipe(gulp.dest(config.cssDir))
+});
+
+gulp.task('watch', function(){
+    watch(config.scssDir + '/**/*.scss', function(){
+        gulp.start('style');
+        console.log(isProduction);
+    });
+});
